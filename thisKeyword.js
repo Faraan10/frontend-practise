a68746014196bdde3009bbbffffffcf0a972f0bc
Muhammed <faraanshaik19@gmail.com>
2025-06-24 01:18:00 +0530

    this keyword commit

---
+++ thisKeyword.js
@@ -0,0 +1,13 @@
+// this refers to the object that is executing the current function.
+// Its value depends on how the function is called, not where it's written."
+
+const obj = {
+  name: "Faraan",
+  arrow: () => console.log(this.name), // ❌ undefined (global this)
+  normal() {
+    console.log(this.name); // ✅ "Faraan"
+  },
+};
+
+obj.arrow();
+obj.normal();
