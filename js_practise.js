50b1b0d07b0e3662ac256d1a7083796b92b7aa84
Muhammed <faraanshaik19@gmail.com>
2025-06-23 15:06:24 +0530

    js file commit

--- js_practise.js
+++ js_practise.js
@@ -1,1 +1,21 @@
 console.log("hello");
+
+// this below will give an error as let is block scoped
+// let a = 10;
+// function test() {
+//   console.log(a);
+//   let a = 5;
+// }
+// test();
+
+const data = async () => {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+data().then((res) => console.log(res));
