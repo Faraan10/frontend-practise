10c1e2480a7d1f206c1b6bddb5cb585e44c35137
Muhammed <faraanshaik19@gmail.com>
2025-06-23 15:10:51 +0530

    js file commit

--- js_practise.js
+++ js_practise.js
@@ -11,8 +11,8 @@ console.log("hello");
 const data = async () => {
   try {
     const response = await fetch("https://fakestoreapi.com/products");
-    const jsonData = await response.json();
-    console.log(jsonData);
+    const resJson = await response.json();
+    console.log(resJson);
   } catch (err) {
     console.log("Error", err);
   }
