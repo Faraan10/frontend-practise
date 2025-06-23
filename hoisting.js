916fc81bb7474391204fd2722a2b81a21dc00d0e
Muhammed <faraanshaik19@gmail.com>
2025-06-23 19:37:46 +0530

    hoisting commit

--- hoisting.js
+++ hoisting.js
@@ -6,14 +6,24 @@
 // let and const exist in the Temporal Dead Zone — if you try to access them
 // before their declaration, you get a ReferenceError.”
 
-// prints undefined
+// prints undefined as it stored as undefined in memory execution phase
+// and assigned at code execution phase
 console.log(a);
 var a = 5;
 
 // referenceError cannot access b before initialization
-console.log(b);
+// console.log(b);
 let b = 10;
 
 // referenceError cannot access c before initialization
-console.log(c);
+// console.log(c);
 let c = 20;
+
+//
+// but the function prints Hello because it is assigned in the memory
+// execution phase
+function greet() {
+  console.log("Hello");
+}
+
+greet();
